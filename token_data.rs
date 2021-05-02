5b17fffeededd0599a967128fd7b561dc6a5e1ce
Douglas Kuhn <bluewitchproductions@gmail.com>
2021-05-02 08:29:13 -0700

    Update token_data.rs

--- token_data.rs
+++ token_data.rs
@@ -4,3 +4,17 @@ struct coins {
     coin_index: String,
     coin_value: u64,
 }
+
+let coin_data = coins {
+        coin_name: String::from("Bitcoin"),
+        coin_index: String::from("BTC"),
+        coin_value: u64,
+    };
+
+fn build_coin(coin_name: String, coin_index: String, coin_value: u64) -> Coins {
+    Coins {
+        coin_name: coin_name,
+        coin_index: coin_index,
+        coin_value: coin_value,
+    }
+}
